d0a7b906da3dc5478816a682a3544daa691c027a
dcentelles <centelld@uji.es>
2017-11-12 18:51:22 +0100

    modify example

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -19,41 +19,78 @@ int main(void) {
     Ptr<IPacketBuilder> pb =
         CreateObject<DataLinkFramePacketBuilder>(checksumType);
     Ptr<CommsDeviceService> comms = CreateObject<CommsDeviceService>(pb);
+    Ptr<CommsDeviceService> rxcomms = CreateObject<CommsDeviceService>(pb);
     comms->SetLogLevel(info);
     comms->SetCommsDeviceId("node0");
     comms->Start();
+
+    //    rxcomms->SetLogLevel(info);
+    //    rxcomms->SetCommsDeviceId("node1");
+    //    rxcomms->Start();
+    //    Ptr<DataLinkFrame> rxdlf = CreateObject<DataLinkFrame>(checksumType);
+    //    char rxmsg[2000];
+
     Ptr<DataLinkFrame> dlf = CreateObject<DataLinkFrame>(checksumType);
-    string msg = "Hello! I'm node0!";
+
+    //    string msg = "Hello! I'm node0!";
+    char msg[5000];
+    int size = 300;
+    for (int i = 0; i < size; i++) {
+      msg[i] = 'a' + i;
+    }
+
+    //    rxcomms->WaitForDeviceReadyToTransmit();
+    //    std::this_thread::sleep_for(chrono::seconds(5));
+    //    while(rxcomms->GetRxFifoSize()>0)
+    //    {
+    //        rxcomms >> rxdlf;
+    //    }
+
+    int cont = 0;
     while (true) {
-      memcpy(dlf->GetPayloadBuffer(), msg.c_str(), msg.size());
-      dlf->PayloadUpdated(msg.size());
-      log->Info("Transmitting packet...");
+
+      msg[0] = '0' + cont;
+      cont = (cont + 1) % 10;
+      memcpy(dlf->GetPayloadBuffer(), msg, size);
+      dlf->PayloadUpdated(size);
+      log->Info("Transmitting packet... {}", msg[0]);
+      log->FlushLog();
       comms << dlf;
-      this_thread::sleep_for(chrono::milliseconds(1000));
+      std::this_thread::sleep_for(chrono::milliseconds(10000));
+
+//      log->Info("Waiting for packet...");
+//      log->FlushLog();
+//      rxcomms >> rxdlf;
+//      memcpy(rxmsg, rxdlf->GetPayloadBuffer(), rxdlf->GetPayloadSize());
+//      rxmsg[100] = 0;
+//      rxmsg[rxdlf->GetPacketSize()] = 0;
+//      log->Info("Packet received!: {}", rxmsg);
+//      log->FlushLog();
     }
 
   });
 
-  std::thread rx([]() {
-    Ptr<Logger> log = CreateObject<Logger>();
-    log->SetLogName("node1");
-    log->SetLogLevel(info);
-    auto checksumType = DataLinkFrame::fcsType::crc16;
-    Ptr<IPacketBuilder> pb =
-        CreateObject<DataLinkFramePacketBuilder>(checksumType);
-    Ptr<CommsDeviceService> comms = CreateObject<CommsDeviceService>(pb);
-    comms->SetLogLevel(info);
-    comms->SetCommsDeviceId("node1");
-    comms->Start();
-    Ptr<DataLinkFrame> dlf = CreateObject<DataLinkFrame>(checksumType);
-    char msg[100];
-    while (true) {
-      comms >> dlf;
-      memcpy(msg, dlf->GetPayloadBuffer(), dlf->GetPayloadSize());
-      msg[dlf->GetPacketSize()] = 0;
-      log->Info("Packet received!: {}", msg);
-    }
-  });
+    std::thread rx([]() {
+      Ptr<Logger> log = CreateObject<Logger>();
+      log->SetLogName("node1");
+      log->SetLogLevel(info);
+      auto checksumType = DataLinkFrame::fcsType::crc16;
+      Ptr<IPacketBuilder> pb =
+          CreateObject<DataLinkFramePacketBuilder>(checksumType);
+      Ptr<CommsDeviceService> comms = CreateObject<CommsDeviceService>(pb);
+      comms->SetLogLevel(info);
+      comms->SetCommsDeviceId("node1");
+      comms->Start();
+      Ptr<DataLinkFrame> dlf = CreateObject<DataLinkFrame>(checksumType);
+      char msg[5000];
+      while (true) {
+        comms >> dlf;
+        memcpy(msg, dlf->GetPayloadBuffer(), dlf->GetPayloadSize());
+        msg[100] = 0;
+        msg[dlf->GetPacketSize()] = 0;
+        log->Info("Packet received!: {}", msg);
+      }
+    });
 
   while (1) {
     this_thread::sleep_for(chrono::milliseconds(5000));
