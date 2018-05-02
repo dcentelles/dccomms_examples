9fc15b165a885c3aac4f863990b813e03985a7d2
dcentelles <centelld@uji.es>
2018-05-02 19:37:51 +0200

    update example

--- src/main/example5.cpp
+++ src/main/example5.cpp
@@ -65,7 +65,7 @@ int main(int argc, char **argv) {
   log->SetLogName(nodeName);
   log->LogToConsole(false);
 
-  node->SetLogLevel(info);
+  node->SetLogLevel(warn);
   node->Start();
 
   std::thread tx, rx;
